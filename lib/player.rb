a7dcd23cc1b13a4598cd87920a0e291b276b612e
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-01-18 21:01:47 -0800

    Add attribute to hold the guessing word value

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal = false
 
 class Player
-  attr_accessor :name, :guess
+  attr_accessor :name, :guess, :word_guess, :letter_guess
 
   def initialize(name = "Player")
     self.name = name
-    self.guess = ''
+    self.letter_guess = ''
+    self.word_guess = ''
   end
 end
